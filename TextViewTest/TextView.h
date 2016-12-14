1ccb1b5022c3f88cb25b7d9a17afd1ebb1232202
Vignesh Ramesh <vignesh-2044@vignesh-2044.local>
2016-12-14 17:38:16 +0530

    TextView class added(confirms to uitextinput).

---
+++ TextViewTest/TextView.h
@@ -0,0 +1,13 @@
+//
+//  TextView.h
+//  TextViewTest
+//
+//  Created by Vignesh Ramesh on 25/11/16.
+//  Copyright © 2016 Vignesh Ramesh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TextView : UIView <UITextInput>
+
+@end
--- TextViewTest/ViewController.m
+++ TextViewTest/ViewController.m
@@ -7,8 +7,13 @@
 //
 
 #import "ViewController.h"
+#import "TextView.h"
 
-@interface ViewController ()
+@interface ViewController ()<UITextViewDelegate>
+{
+    TextView *textView;
+//    UITextView *textView;
+}
 
 @end
 
@@ -17,12 +22,63 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+//    textView = [UITextView new];
+//    textView.delegate = self;
+    
+    textView = [[TextView alloc] init];
+    
+    CGRect frame = self.view.frame;
+    frame.origin.y += 20;
+    textView.frame = frame;
+
+    [self.view addSubview:textView];
 }
 
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(BOOL)textViewShouldBeginEditing:(UITextView *)textView
+{
+    return YES;
+}
+
+-(void)textViewDidBeginEditing:(UITextView *)textView
+{
+    
+}
+
+-(BOOL)textViewShouldEndEditing:(UITextView *)textView
+{
+    return YES;
+}
+
+-(void)textViewDidEndEditing:(UITextView *)textView
+{
+    
+}
+
+-(BOOL)textView:(UITextView *)textView shouldChangeTextInRange:(NSRange)range replacementText:(NSString *)text
+{
+    return YES;
+}
+
+-(void)textViewDidChangeSelection:(UITextView *)textView
+{
+    
+}
+
+-(void)textViewDidChange:(UITextView *)textView
+{
+    
 }
 
 
